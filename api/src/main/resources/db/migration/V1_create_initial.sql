a45d4808b894e64a9470fa006fe4e6f6f18c4420
RichardOrlandini <richardorlandini09@gmail.com>
2023-09-28 17:29:43 -0300

    Refatora Insert de ADMS

--- api/src/main/resources/db/migration/V1_create_initial.sql
+++ api/src/main/resources/db/migration/V1_create_initial.sql
@@ -23,13 +23,7 @@ CREATE TABLE usuario (
 
 );
 
-insert into usuario values(1, TRUE, 'adm@teste', true,  'adm', '$2a$10$Y50UaMFOxteibQEYLrwuHeehHYfcoafCopUazP12.rqB41bsolF5.'
-, '11987634487',  NULL);
--- senha 123456
 
---INSERT INTO usuario values
---(id, active, cpf, email, nome, "role", senha, telefone, endereco_id, confirm_password)
---VALUES(nextval('usuario_id_seq'::regclass), false, '45214479883', 'digao@', 'testando', 0, '54321', '11934483009',0, '54321');
 CREATE TABLE cliente (
     id                       bigint NOT NULL,
     active                   boolean NOT NULL,
--- api/src/main/resources/db/migration/V2_insert_usuario.sql
+++ api/src/main/resources/db/migration/V2_insert_usuario.sql
@@ -1,5 +1,4 @@
 
-insert into usuario values(1, TRUE, 'adm@teste', true,  'adm', '$2a$10$Y50UaMFOxteibQEYLrwuHeehHYfcoafCopUazP12.rqB41bsolF5.'
+insert into usuario values(1, TRUE, '5410322208876' ,'adm@teste', true,  0, '$2a$10$Y50UaMFOxteibQEYLrwuHeehHYfcoafCopUazP12.rqB41bsolF5.'
 , '11987634487',  NULL);
--- senha 123456
---INSERT INTO endereco VALUES (1, 'Rua Saloá', '123', 'Rua', 'Jardim Mutinga','05159-040');
+
