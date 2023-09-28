165af70dd752c305025095e56b4b0685291d2a52
RichardOrlandini <richardorlandini09@gmail.com>
2023-09-28 19:57:34 -0300

    Altera insert novamente

--- api/src/main/resources/db/migration/V2_insert_usuario.sql
+++ api/src/main/resources/db/migration/V2_insert_usuario.sql
@@ -1,4 +1,4 @@
-
+DELETE FROM public.usuario WHERE id=1;
 insert into usuario values(1, TRUE, '5410322208876' ,'adm@teste', true,  0, '$2a$10$Y50UaMFOxteibQEYLrwuHeehHYfcoafCopUazP12.rqB41bsolF5.'
 , '11987634487',  NULL);
 
