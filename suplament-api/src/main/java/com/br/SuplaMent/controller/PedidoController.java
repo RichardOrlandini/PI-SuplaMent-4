a0fa2a81301c7ebeb5eaabaf63972b87ed0e0fcc
RichardOrlandini <richardorlandini09@gmail.com>
2023-11-06 12:51:45 -0300

    Ajusta controller de pedidos

--- suplament-api/src/main/java/com/br/SuplaMent/controller/PedidoController.java
+++ suplament-api/src/main/java/com/br/SuplaMent/controller/PedidoController.java
@@ -6,10 +6,8 @@ import com.br.SuplaMent.domain.pedido.dto.CreatePedidoDTO;
 import com.br.SuplaMent.domain.pedido.dto.ListagemPedidosDTO;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,13 +20,16 @@ public class PedidoController {
     private final PedidoService pedidoService;
 
     @PostMapping
+    @Transactional
     public AvisoRetornoPedidoDTO save(@RequestBody CreatePedidoDTO createPedidoDTO ){
         AvisoRetornoPedidoDTO avisoRetornoPedidoDTO = pedidoService.save(createPedidoDTO);
         return avisoRetornoPedidoDTO;
     }
     //Procurar pedidos pelo id do cliente
-    public List<ListagemPedidosDTO> findAll(@RequestBody Long idCliente){
-        List<ListagemPedidosDTO> listaPedidos = pedidoService.buscarPedidosDoCliente(idCliente);
+
+    @GetMapping("{id}")
+    public List<ListagemPedidosDTO> findAll(@PathVariable Long id){
+        List<ListagemPedidosDTO> listaPedidos = pedidoService.buscarPedidosDoCliente(id);
         return listaPedidos;
     }
 }
