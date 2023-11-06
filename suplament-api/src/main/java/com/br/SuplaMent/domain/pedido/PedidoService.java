7e1984d0b7c18e72864c19876e46df6c9402673b
RichardOrlandini <richardorlandini09@gmail.com>
2023-11-06 12:52:06 -0300

    Ajusta logica em save pedido

--- suplament-api/src/main/java/com/br/SuplaMent/domain/pedido/PedidoService.java
+++ suplament-api/src/main/java/com/br/SuplaMent/domain/pedido/PedidoService.java
@@ -4,6 +4,8 @@ import com.br.SuplaMent.domain.pedido.dto.AvisoRetornoPedidoDTO;
 import com.br.SuplaMent.domain.pedido.dto.CreatePedidoDTO;
 import com.br.SuplaMent.domain.pedido.dto.ListagemPedidosDTO;
 import com.br.SuplaMent.domain.pessoa.Cliente;
+import com.br.SuplaMent.domain.pessoa.ClienteRepository;
+import com.br.SuplaMent.infra.exception.ValidationExcepetion;
 import com.br.SuplaMent.utils.enums.FormaPagamento;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,20 +19,32 @@ import java.util.stream.Collectors;
 public class PedidoService {
     @Autowired
     private PedidoRepository pedidoRepository;
+
+    @Autowired
+    private ClienteRepository clienteRepository;
     public AvisoRetornoPedidoDTO save(CreatePedidoDTO createPedidoDTO) {
         FormaPagamento formaPagamento;
         String mensagem = null;
         try {
-            formaPagamento = FormaPagamento.valueOf(createPedidoDTO.formaPagamento());
+            formaPagamento = createPedidoDTO.formaPagamento();
         } catch (IllegalArgumentException e) {
             formaPagamento = null;
             mensagem = "\nA FORMA DE PAGAMENTO INFORMADA NÃO EXISTE!\n";
             return new AvisoRetornoPedidoDTO(ThreadLocalRandom.current().nextLong(), createPedidoDTO.valorTotal(), mensagem);
         }
+        Cliente cliente = this.validateClient(createPedidoDTO.idCliente());
         //BUSCAR O CLIENTE PELO ID QUE ESTA VINDO PELO DTO, E MANDAR O CLIENTE NESSE PARÂMETRO
-        Pedido pedido = Pedido.of(createPedidoDTO, new Cliente(), formaPagamento);
+        Pedido pedido = Pedido.of(createPedidoDTO, cliente, formaPagamento);
         pedidoRepository.save(pedido);
         return new AvisoRetornoPedidoDTO(pedido.getId(), pedido.getValorTotal(), mensagem);
+    }
+
+    private Cliente validateClient(Long clienteId) {
+        Cliente cliente = clienteRepository.findById(clienteId).orElse(null);
+        if (cliente == null) {
+            new ValidationExcepetion("O client informado no pedido não existe!");
+        }
+        return cliente;
     }
 
     public List<ListagemPedidosDTO> buscarPedidosDoCliente(Long clienteId) {
