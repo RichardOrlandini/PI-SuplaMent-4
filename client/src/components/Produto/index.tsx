76c8654518caf19fc2494c1e51615ded6e789bf2
rodrigo Junior <77363230+rodrigojunior07@users.noreply.github.com>
2023-11-29 16:47:16 -0300

    add

--- client/src/components/Produto/index.tsx
+++ client/src/components/Produto/index.tsx
@@ -6,18 +6,25 @@ import { memo, useContext } from 'react';
 import { useCarrinhoContext } from 'common/contexts/Carrinho';
 import { UsuarioContext } from 'common/contexts/Usuario';
 import { IProduto } from 'shared/interfaces/IProduto';
+import { useNavigate } from 'react-router-dom';
 
 const Produto: React.FC<IProduto> = (produto: IProduto) => {
   const { carrinho, adicionarProduto, removerProduto, valorTotal } = useCarrinhoContext();
   const { saldo } = useContext(UsuarioContext);
-
+  const navigate = useNavigate();
   const pathImages = "../../../public/images";
   const itemNoCarrinho = carrinho.find(item => item.produto.id === produto.id);
+
+
+  const handleDetailsProdutos = (id : number) => {
+    navigate(`/${id}`);
+  }
 
   return (
     <Card>
       <CardMedia
         component="img"
+        onClick={() => handleDetailsProdutos(produto.id)}
         image={`${pathImages}/${produto.nomeImagem}`}
         alt={`foto de ${produto.nome}`}
         style={{ height: '200px', width: '200px', objectFit: 'cover' }} // define um tamanho fixo para todas as imagens
