6e420f91654d9b1a968e88f7bdc89050c191e714
rodrigo Junior <77363230+rodrigojunior07@users.noreply.github.com>
2023-12-12 21:51:19 -0300

    import arrumado

--- suplament-api/src/main/resources/import.sql
+++ suplament-api/src/main/resources/import.sql
@@ -10,26 +10,26 @@ INSERT INTO FORNECEDOR (ID, NOME) VALUES (1000, 'Suplament');
 -- Inserindo produtos
 
 -- Whey
-INSERT INTO public.produto (active, qtd, valor, fk_categoria, fk_fornecedor, id, insertion_date, nome, nome_imagem) VALUES (true, 20, 30.0, 1000, 1000, 1000, 'now()', 'Whey 1', 'whey1.jpg');
-INSERT INTO public.produto (active, qtd, valor, fk_categoria, fk_fornecedor, id, insertion_date, nome, nome_imagem) VALUES (true, 20, 35.0, 1000, 1000, 1001, 'now()', 'Whey 2', 'whey2.jpg');
-INSERT INTO public.produto (active, qtd, valor, fk_categoria, fk_fornecedor, id, insertion_date, nome, nome_imagem) VALUES (true, 20, 40.0, 1000, 1000, 1002, 'now()', 'Whey 3', 'whey3.jpg');
-INSERT INTO public.produto (active, qtd, valor, fk_categoria, fk_fornecedor, id, insertion_date, nome, nome_imagem) VALUES (true, 20, 45.0, 1000, 1000, 1003, 'now()', 'Whey 4', 'whey4.jpg');
-INSERT INTO public.produto (active, qtd, valor, fk_categoria, fk_fornecedor, id, insertion_date, nome, nome_imagem) VALUES (true, 20, 50.0, 1000, 1000, 1004, 'now()', 'Whey 5', 'whey5.jpg');
+INSERT INTO public.produto (active, qtd, valor, fk_categoria, fk_fornecedor, id, insertion_date, nome, nome_imagem) VALUES (true, 20, 30.0, 1000, 1000, 1000, now(), 'Whey 1', 'whey1.jpg');
+INSERT INTO public.produto (active, qtd, valor, fk_categoria, fk_fornecedor, id, insertion_date, nome, nome_imagem) VALUES (true, 20, 35.0, 1000, 1000, 1001, now(), 'Whey 2', 'whey2.jpg');
+INSERT INTO public.produto (active, qtd, valor, fk_categoria, fk_fornecedor, id, insertion_date, nome, nome_imagem) VALUES (true, 20, 40.0, 1000, 1000, 1002, now(),'Whey 3', 'whey3.jpg');
+INSERT INTO public.produto (active, qtd, valor, fk_categoria, fk_fornecedor, id, insertion_date, nome, nome_imagem) VALUES (true, 20, 45.0, 1000, 1000, 1003, now(), 'Whey 4', 'whey4.jpg');
+INSERT INTO public.produto (active, qtd, valor, fk_categoria, fk_fornecedor, id, insertion_date, nome, nome_imagem) VALUES (true, 20, 50.0, 1000, 1000, 1004, now(), 'Whey 5', 'whey5.jpg');
 
 -- Barras
 
-INSERT INTO public.produto (active, qtd, valor, fk_categoria, fk_fornecedor, id, insertion_date, nome, nome_imagem) VALUES (true, 20, 10.0, 1001, 1000, 1005, 'now()', 'Barra 1', 'barra1.jpg');
-INSERT INTO public.produto (active, qtd, valor, fk_categoria, fk_fornecedor, id, insertion_date, nome, nome_imagem) VALUES (true, 20, 15.0, 1001, 1000, 1006, 'now()', 'Barra 2', 'barra2.jpg');
-INSERT INTO public.produto (active, qtd, valor, fk_categoria, fk_fornecedor, id, insertion_date, nome, nome_imagem) VALUES (true, 20, 20.0, 1001, 1000, 1007, 'now()', 'Barra 3', 'barra3.jpg');
-INSERT INTO public.produto (active, qtd, valor, fk_categoria, fk_fornecedor, id, insertion_date, nome, nome_imagem) VALUES (true, 20, 25.0, 1001, 1000, 1008, 'now()', 'Barra 4', 'barra4.jpg');
-INSERT INTO public.produto (active, qtd, valor, fk_categoria, fk_fornecedor, id, insertion_date, nome, nome_imagem) VALUES (true, 20, 30.0, 1001, 1000, 1009, 'now()', 'Barra 5', 'barra5.jpg');
+INSERT INTO public.produto (active, qtd, valor, fk_categoria, fk_fornecedor, id, insertion_date, nome, nome_imagem) VALUES (true, 20, 10.0, 1001, 1000, 1005, now(), 'Barra 1', 'barra1.jpg');
+INSERT INTO public.produto (active, qtd, valor, fk_categoria, fk_fornecedor, id, insertion_date, nome, nome_imagem) VALUES (true, 20, 15.0, 1001, 1000, 1006, now(), 'Barra 2', 'barra2.jpg');
+INSERT INTO public.produto (active, qtd, valor, fk_categoria, fk_fornecedor, id, insertion_date, nome, nome_imagem) VALUES (true, 20, 20.0, 1001, 1000, 1007, now(), 'Barra 3', 'barra3.jpg');
+INSERT INTO public.produto (active, qtd, valor, fk_categoria, fk_fornecedor, id, insertion_date, nome, nome_imagem) VALUES (true, 20, 25.0, 1001, 1000, 1008, now(), 'Barra 4', 'barra4.jpg');
+INSERT INTO public.produto (active, qtd, valor, fk_categoria, fk_fornecedor, id, insertion_date, nome, nome_imagem) VALUES (true, 20, 30.0, 1001, 1000, 1009, now(), 'Barra 5', 'barra5.jpg');
 -- Camisetas
 
-INSERT INTO public.produto (active, qtd, valor, fk_categoria, fk_fornecedor, id, insertion_date, nome, nome_imagem) VALUES (true, 20, 50.0, 1002, 1000, 1010, 'now()', 'Camiseta 1', 'camiseta1.jpg');
-INSERT INTO public.produto (active, qtd, valor, fk_categoria, fk_fornecedor, id, insertion_date, nome, nome_imagem) VALUES (true, 20, 55.0, 1002, 1000, 1011, 'now()', 'Camiseta 2', 'camiseta2.jpg');
-INSERT INTO public.produto (active, qtd, valor, fk_categoria, fk_fornecedor, id, insertion_date,nome, nome_imagem) VALUES (true, 20, 60.0, 1002, 1000, 1012, 'now()', 'Camiseta 3', 'camiseta3.jpg');
-INSERT INTO public.produto (active, qtd, valor, fk_categoria, fk_fornecedor, id, insertion_date,nome, nome_imagem) VALUES (true, 20, 65.0, 1002, 1000, 1013, 'now()', 'Camiseta 4', 'camiseta4.jpg');
-INSERT INTO public.produto (active, qtd, valor, fk_categoria, fk_fornecedor, id, insertion_date,nome, nome_imagem) VALUES (true, 20, 70.0, 1002, 1000, 1014, 'now()', 'Camiseta 5', 'camiseta5.jpg');
+INSERT INTO public.produto (active, qtd, valor, fk_categoria, fk_fornecedor, id, insertion_date, nome, nome_imagem) VALUES (true, 20, 50.0, 1002, 1000, 1010, now(), 'Camiseta 1', 'camiseta1.jpg');
+INSERT INTO public.produto (active, qtd, valor, fk_categoria, fk_fornecedor, id, insertion_date, nome, nome_imagem) VALUES (true, 20, 55.0, 1002, 1000, 1011, now(), 'Camiseta 2', 'camiseta2.jpg');
+INSERT INTO public.produto (active, qtd, valor, fk_categoria, fk_fornecedor, id, insertion_date,nome, nome_imagem) VALUES (true, 20, 60.0, 1002, 1000, 1012, now(), 'Camiseta 3', 'camiseta3.jpg');
+INSERT INTO public.produto (active, qtd, valor, fk_categoria, fk_fornecedor, id, insertion_date,nome, nome_imagem) VALUES (true, 20, 65.0, 1002, 1000, 1013, now(), 'Camiseta 4', 'camiseta4.jpg');
+INSERT INTO public.produto (active, qtd, valor, fk_categoria, fk_fornecedor, id, insertion_date,nome, nome_imagem) VALUES (true, 20, 70.0, 1002, 1000, 1014, now(), 'Camiseta 5', 'camiseta5.jpg');
 
 
 
