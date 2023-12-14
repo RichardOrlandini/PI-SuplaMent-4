1d08cc345de8f625d0fed6072cd4f0cc0c14a502
rodrigo Junior <77363230+rodrigojunior07@users.noreply.github.com>
2023-12-14 11:58:04 -0300

    Update CepService.java

--- suplament-api/src/main/java/com/br/SuplaMent/services/CepService.java
+++ suplament-api/src/main/java/com/br/SuplaMent/services/CepService.java
@@ -12,7 +12,13 @@ public class CepService {
     private static final String API_URL = "https://viacep.com.br/ws/{cep}/json";
 
     //"https://viacep.com.br/ws/" + cep + "/json"
+
     public static boolean fazValidaCep(String cep) {
+        // Verifique se o CEP é válido
+        if (!cep.matches("\\d{5}-\\d{3}")) {
+            throw new IllegalArgumentException("CEP inválido: " + cep);
+        }
+
         RestTemplate restTemplate = new RestTemplate();
         String url = UriComponentsBuilder.fromUriString(API_URL)
                 .buildAndExpand(cep)
@@ -21,9 +27,14 @@ public class CepService {
             restTemplate.getForObject(url, String.class);
             return true;
         } catch (Exception e) {
+            System.out.println("Error validating CEP: " + e.getMessage());
+            e.printStackTrace();
+
             return false;
         }
     }
+
+
 
     public static Endereco BuscaCepDetalhes(String cep) {
         RestTemplate restTemplate = new RestTemplate();
