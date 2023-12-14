ba40635363a021a9aeea067b1c2d1744a7d726d0
rodrigo Junior <77363230+rodrigojunior07@users.noreply.github.com>
2023-12-14 11:50:02 -0300

    Update EnderecoController.java
    mudei

--- suplament-api/src/main/java/com/br/SuplaMent/controller/EnderecoController.java
+++ suplament-api/src/main/java/com/br/SuplaMent/controller/EnderecoController.java
@@ -1,5 +1,6 @@
 package com.br.SuplaMent.controller;
 
+
 import com.br.SuplaMent.domain.endereco.Endereco;
 import com.br.SuplaMent.domain.pessoa.Cliente;
 import com.br.SuplaMent.domain.pessoa.dto.CadastroEnderecosDTO;
@@ -21,25 +22,17 @@ public class EnderecoController {
 
     final EnderecoService enderecoService;
 
-//    @PostMapping
-//    public ResponseEntity<DetalhamentoClienteDTO> save(@RequestBody List<CadastroEnderecosDTO> enderecosDTOS, UriComponentsBuilder uriBuilder) {
-//        CadastroEnderecosDTO dto = enderecosDTOS.get(0);
-//        Endereco enderecoSalvo = (Endereco) CepService.BuscaCepDetalhes(String.valueOf(dto));
-//        enderecoService.save(enderecosDTOS);
-//        var uri = uriBuilder.path("/cliente/{id}").buildAndExpand(enderecoSalvo.getId()).toUri();
-//        return ResponseEntity.created(uri).body(new DetalhamentoClienteDTO(enderecoSalvo.getCliente()));
-//    }
-
-    @GetMapping("/consulta")
+    @GetMapping("consulta")
     public  ResponseEntity consultaEndereco(@RequestBody Cliente clienteEndereco){
         return ResponseEntity.ok(CepService.fazValidaCep(String.valueOf(clienteEndereco)));
 
     }
-    @PutMapping("/enderecos/principal")
+    @PutMapping("principal")
     public ResponseEntity<Endereco> setPrincipalAddress(@RequestBody Long addId) {
         enderecoService.setPrincipalAddress(addId);
         return new ResponseEntity<>(HttpStatus.OK);
     }
+
 
 //    @GetMapping("/status")
 //    public String get(){
