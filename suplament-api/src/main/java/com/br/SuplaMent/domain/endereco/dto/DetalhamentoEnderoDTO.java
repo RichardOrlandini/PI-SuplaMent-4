3c328ceed8f3a52387ba6093d8548d5a7b0b1f13
rodrigo Junior <77363230+rodrigojunior07@users.noreply.github.com>
2023-12-14 11:52:54 -0300

    Update DetalhamentoEnderoDTO.java

--- suplament-api/src/main/java/com/br/SuplaMent/domain/endereco/dto/DetalhamentoEnderoDTO.java
+++ suplament-api/src/main/java/com/br/SuplaMent/domain/endereco/dto/DetalhamentoEnderoDTO.java
@@ -5,9 +5,9 @@ import com.br.SuplaMent.domain.endereco.Endereco;
 import com.br.SuplaMent.domain.pessoa.Cliente;
 
 
-public record DetalhamentoEnderoDTO (Long id, String complemento, String numero, String logradouro, String bairro, String cep, String cidade, String uf,Cliente cliente) {
+public record DetalhamentoEnderoDTO (Long id, String complemento, String numero, String logradouro, String bairro, String cep, String cidade, String uf,Boolean principal,Cliente cliente) {
 
     public DetalhamentoEnderoDTO (Endereco endereco) {
-        this(endereco.getId(), endereco.getComplemento(), endereco.getNumero(), endereco.getLogradouro(), endereco.getBairro(), endereco.getCep(), endereco.getCidade(), endereco.getUf(),endereco.getCliente());
+        this(endereco.getId(), endereco.getComplemento(), endereco.getNumero(), endereco.getLogradouro(), endereco.getBairro(), endereco.getCep(), endereco.getCidade(), endereco.getUf(), endereco.isPrincipal(), endereco.getCliente());
     }
 }
